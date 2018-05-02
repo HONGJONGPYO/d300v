5cf886217b241aee92309a895a89f2f3a2a2f49a
stc_180-PC\stc_180 <whdxy12@gmail.com>
2018-05-02 14:02:52 +0900

    주석추가

--- main.c
+++ main.c
@@ -11,7 +11,7 @@
 void l_sh();
 void r_sh();
 unsigned char a;
-
+///////////////////////////////////////////////////////////////////////
 int main(void)
 {
     /* Replace with your application code */
